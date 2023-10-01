603fa50a063e6e0207f4eda4cb67f439b5608786
wbrewster02 <wbrewster02@gmail.com>
2023-10-01 18:02:37 -0400

    updated routes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 // routes
-app.use('/user', springController)
+app.use('/spring', springController)
 
 // db connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
--- models/Spring.js
+++ models/Spring.js
@@ -5,11 +5,19 @@ const SpringSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    email: {
-        type: String,
-        required: true,
-        
+    lotdate: {
+        type: Date,
+        required: true
     },
+    type: {
+        type: String,
+        required: true
+    },
+    initialCount: {
+        type: Number,
+        required: true
+    }
+
 })
 
 module.exports = mongoose.model('Spring', SpringSchema)
